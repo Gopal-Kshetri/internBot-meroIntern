03af789df1b8c0fbd51a9d72c488450769939c18
Gopal-Kshetri <075bct039.gopal@pcampus.edu.np>
2022-05-27 00:23:50 +0545

    help command styled

--- bot.py
+++ bot.py
@@ -15,25 +15,34 @@ async def on_ready():
 
 @bot.command(name='help', help='Commands for the Discord bot')
 async def help(ctx):
-    await ctx.send(
-"""```Help    Commands for the Discord bot
-gg      Responds with a random quote from Friends
-roast   Roasts you with mom joke
-interns  Provides you the available interns```"""
-    )
+    embed=discord.Embed(
+        title="Help", 
+        description="Commands for botSoy", 
+        color=discord.Color.orange()
+        )
+    embed.set_author(
+        name=ctx.author.display_name, 
+        url="https://twitter.com/RealDrewData", 
+        icon_url=ctx.author.avatar_url
+        )
+    embed.set_thumbnail(url="https://merointernship.com/wp-content/uploads/2021/09/coverimages-01-630x230.jpg")
 
-# @bot.command(name='interns', help='Provides you the available interns')
-# async def interns(ctx):
-#     titles, authors, dates = intern_meroIntern()
-#     response = PrettyTable(['Title', 'Author', 'Date'])
-#     for (title, author, date) in zip(titles, authors, dates):
-#         # print(title+ '\t' + author + '\t' + date + '\t')
-#         response.add_row([title, author, date])
-#     await ctx.send(response)
+    bot_commands = ['help', 'gg', 'roast', 'interns']
+    descriptions = ['Displays commands for the Discord bot', 'Responds with a random quote from Friends', 'Roasts you with Yo mama joke', 'Provides you the available interns']
+    for (bot_command,description) in zip(bot_commands, descriptions):
+        embed.add_field(name=f'**{bot_command}**', value=f'> {description}', inline=False)
+
+    await ctx.send(embed=embed)
+#     await ctx.send(
+# """```Help    Commands for the Discord bot
+# gg      Responds with a random quote from Friends
+# roast   Roasts you with Yo mama joke
+# interns  Provides you the available interns```"""
+#     )
 
 
 @bot.command(name = 'interns', help='Embed in Discord')
-async def embed(ctx):
+async def interns(ctx):
     embed=discord.Embed(
         title="Mero Internship", 
         url="https://merointernship.com/category/internship/", 
@@ -117,7 +126,16 @@ async def roast(ctx):
         'Yo Mama\'s like a library, open to the public.',
         'Yo Mama\'s like mustard, she spreads easy.',
         'If Yo Mama and Yo Daddy got a divorce, they\'d still be brother and sister.',
-        'Yo Mama\'s so fat her butt cheeks have different area codes.'
+        'Yo Mama\'s so fat her butt cheeks have different area codes.',
+        'Yo mama\'s so stupid, she put lipstick on her forehead to make up her mind.',
+        'Yo mama\'s so stupid, she took a ruler to bed to see how long she slept.',
+        'Yo mama\'s so ugly, her birth certificate is an apology letter.',
+        'Yo mama\'s so ugly, she threw a boomerang and it refused to come back.',
+        'Yo mama\'s cooking so nasty, she flys got together and fixed the hole in the window screen.',
+        'Yo mama\'s so nasty, they used to call them jumpolines \'til yo mama bounced on one.',
+        'Yo mama\'s so lazy, she stuck her nose out the window and let the wind blow it.',
+        'Yo mama\'s so dumb, she went to the eye doctor to get an iPhone.',
+        'Yo mama\'s so literal, she stared at a cup of orange juice for 12 hours because it said, “concentrate.”',
     ]
 
     response = random.choice(mom_jokes)
